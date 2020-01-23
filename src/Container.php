4c075420b7cbfcc66b571877d468ce04faecdc90
Egor Vasyakin <evasyakin@mail.ru>
2020-01-24 00:18:40 +0700

    fix phpdoc, add DI methods

--- src/Container.php
+++ src/Container.php
@@ -1,16 +1,16 @@
 <?php
 /**
-* @package evas-php/evas-di
-*/
+ * @package evas-php/evas-di
+ */
 namespace Evas\Di;
 
 use Evas\Di\ContainerTrait;
 
 /**
-* Di-контейнер класс.
-* @author Egor Vasyakin <e.vasyakin@itevas.ru>
-* @since 1.0
-*/
+ * Di-контейнер класс.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
 class Container
 {
     use ContainerTrait;
--- src/ContainerTrait.php
+++ src/ContainerTrait.php
@@ -1,39 +1,120 @@
 <?php
 /**
-* @package evas-php/evas-di
-*/
+ * @package evas-php/evas-di
+ */
 namespace Evas\Di;
 
 use \Exception;
 
 /**
-* Di-контейнер трейт.
-* @author Egor Vasyakin <e.vasyakin@itevas.ru>
-* @since 1.0
-*/
+ * Di-контейнер трейт.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
 trait ContainerTrait
 {
     // методы
 
+    /**
+     * Установка параметра в контейнер.
+     * @param string|array|object имя параметра или массив/объект параметров
+     * @param mixed|null значение параметра или null
+     */
     public function set($name, $value = null)
     {
+        assert(is_string($name) || is_array($name) || is_object($name));
         if (is_string($name)) {
             $this->$name = $value;
         } else if (is_array($name) || is_object($name)) {
             foreach ($name as $subname => $value) {
                 $this->set($subname, $value);
             }
-        } else {
-            throw new Exception('Argument 1 for set() must be string or array or object');
         }
     }
 
+    /**
+     * Удаление параметра из контейнера.
+     * @param string|array имя параметра или массив имен параметров
+     */
+    public function unset($name)
+    {
+        assert(is_string($name) || is_array($name));
+        if (is_string($name)) {
+            unset($this->$name);
+        } else if (is_array($name)) {
+            foreach ($name as &$subname) {
+                unset($this->$subname);
+            }
+        }
+    }
+
+    /**
+     * Проверка наличия параметра или параметров.
+     * @param string|array имя параметра или массив имен парамтеров
+     * @param mixed значение, если параметр найден
+     * @param mixed значение, если параметр не найден
+     * @return mixed
+     */
+    public function has($name, $defaultIfHas = true, $defaultIfNotHas = false)
+    {
+        assert(is_string($name) || is_array($name));
+        if (is_string($name)) {
+            return isset($this->$name) ? $defaultIfHas : $defaultIfNotHas;
+        } else if (is_array($name)) {
+            $vars = [];
+            foreach ($name as $subname) {
+                $vars[$subname] = $this->has($subname, $defaultIfHas, $defaultIfNotHas);
+            }
+            return $vars;
+        }
+    }
+
+    /**
+     * Получение параметра или массива параметров из контейнера.
+     * @param string|array|object имя параметра 
+     *                            или массив имен параметров 
+     *                            или объект имен со значениями по умолчанию
+     * @return mixed|array of mixed
+     */
+    public function get($name, $default = null)
+    {
+        assert(is_string($name) || is_array($name) || is_object($name));
+        if (is_string($name)) {
+            return $this->has($name) ? $this->$name : $default;
+        } else if (is_array($name)) {
+            $vars = [];
+            foreach ($name as &$subname) {
+                $vars[$subname] = $this->get($subname, $default);
+            }
+            return $vars;
+        } else if (is_object($name)) {
+            $vars = [];
+            foreach ($name as $subname => $default) {
+                $vars[$subname] = $this->get($subname, $default);
+            }
+            return $vars;
+        }
+    }
+
+    /**
+     * Вызов параметра-метода контейнера.
+     * @param string имя параметра
+     * @param array массив аргументов
+     * @return mixed результат выполнения метода
+     */
+    public function call(string $name, array $arguments = [])
+    {
+        $var = $this->__call($name, $arguments);
+    }
+
+
+
     // магия
 
-    public function __construct(array $params = null)
+    public function __construct(array $vars = null)
     {
-        if ($params) {
-            $this->set($params);
+        if ($vars) {
+            $this->set($vars);
         }
     }
 
@@ -52,8 +133,9 @@ trait ContainerTrait
 
     public function __call(string $name, array $arguments = [])
     {
-        if (isset($this->$name)) {
-            return call_user_func_array($this->$name, $arguments);
+        $var = $this->$name;
+        if ($var instanceof \Closure) {
+            return call_user_func_array($var, $arguments);
         }
     }
 }
