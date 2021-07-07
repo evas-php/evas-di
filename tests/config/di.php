b418679a51afc46250b2c374ebc28e18e40c51fb
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-07 09:34:08 +0300

    Create di.php

---
+++ tests/config/di.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Конфигурация Di для тестов.
+ */
+use Evas\Di;
+
+use Evas\Web\WebRequest;
+use Evas\Web\WebResponse;
+use Evas\Router\Controller;
+
+return [
+    'request' => Di\createOnce(WebRequest::class),
+    'response' => Di\createOnce(WebResponse::class),
+    'controller' => Di\create(Controller::class, [
+        Di\get('request')
+    ]),
+
+    'exampleDbConfig' => Di\includeFileOnce(__DIR__ . '/db.example.php'),
+    
+    'author' => 'Egor',
+
+    'authorNameCallback' => function () { return 'Egor'; },
+    'authorName' => Di\call(function () { return 'Egor'; }),
+    'authorNameWithDiContext' => Di\call(function () {
+        return $this->get('author');
+    }),
+
+];
