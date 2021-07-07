25053252efbc75f9d622283e53902c41fa82aaf8
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-07 09:31:47 +0300

    Create DiException.php

---
+++ src/DiException.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Класс исключений модуля Di.
+ * @package evas-php\evas-di
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Di;
+
+class DiException extends \Exception
+{}
